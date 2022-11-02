198c3cbed4e0aff92ad827efb279a9859494ae8a
Stéphane Bollard <74253670+sbyaute@users.noreply.github.com>
2022-11-02 14:02:33 +0100

    Update Controller.tpl.php

--- src/Resources/skeleton/crudmodal/controller/Controller.tpl.php
+++ src/Resources/skeleton/crudmodal/controller/Controller.tpl.php
@@ -8,6 +8,7 @@ use <?= $form_update_full_class_name ?>;
 <?php if (isset($repository_full_class_name)): ?>
 use <?= $repository_full_class_name ?>;
 <?php endif ?>
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\<?= $parent_class_name ?>;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
